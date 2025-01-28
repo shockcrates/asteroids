1c13e704a2a625ab630e37df3a1975f4de95c84c
Anthony Spencer <josephs@smu.edu>
2025-01-27 20:41:36 -0700

    Added triangle rendering of player on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 def main():
 	print("Starting asteroids!")
@@ -9,13 +10,20 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-	while True:
-		screen.fill("black")
-		pygame.display.flip()
+	clock1 = pygame.time.Clock()
+	dt = 0
 
+	bob = player.Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
+		screen.fill("black")
+		bob.draw(screen)
+		pygame.display.flip()
+		dt = clock1.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
